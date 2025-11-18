11cdb6c06b83a48ff749ef1b98e2a2479615f74e
Saxv <vyragootoglobal@gmail.com>
2025-11-18 15:05:12 +0700

    Implement contract address copy and price widget
    Added functionality to copy contract address and embed price widget.
    
    Signed-off-by: Saxv <vyragootoglobal@gmail.com>

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-document.getElementById('hamburger').onclick = () => {
-  const m = document.getElementById('nav-menu');
-  m.style.display = m.style.display === 'block' ? 'none' : 'block';
-};
+const contractAddress = '0x09D85bD22E3d0EbfBCd09166d01Ff880fF14a97b';
+document.getElementById('copy').addEventListener('click', ()=>{navigator.clipboard.writeText(contractAddress)});
+// Price widget embed (DexScreener iframe)
+const widget = document.getElementById('price-widget');
+widget.innerHTML = '<iframe src="https://www.dexscreener.com/iframe/?token=' + contractAddress + '" style="width:100%;height:400px;border:0"></iframe>';
